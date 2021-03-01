7821e178215e4510ee3504ee91e65f7845b2b9a0
Caleb Buxton <me@cpb.ca>
2021-02-28 17:52:27 -0800

    Update settings.py

--- settings.py
+++ settings.py
@@ -4,4 +4,4 @@ load_dotenv()
 
 CONSUMER_KEY = os.getenv("TWITTER_CONSUMER_KEY")
 CONSUMER_SECRET = os.getenv("TWITTER_CONSUMER_SECRET")
-OAUTH_TOKEN, OAUTH_TOKEN_SECRET = read_token_file(os.getenv("TWITTER_OAUTH_PATH")
+OAUTH_TOKEN, OAUTH_TOKEN_SECRET = read_token_file(os.getenv("TWITTER_OAUTH_PATH"))
